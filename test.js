51d30924a2c94c4c02bc253b7885977023c1e408
evssh <44243367+evssh@users.noreply.github.com>
2022-09-28 12:03:41 +0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+    //таймер поставил для наглядности изменений, по ТЗ он не нужен
+    setTimeout( () => {
+        document.querySelector('title').innerHTML = 'Тестовое задание успешно выполнено';
+    }, 300)
+
+}, false);
+
+/*
+if (/[А-Яа-я]/) {
+    //запрет
+} else {
+    //разрешено
+}
+*/
